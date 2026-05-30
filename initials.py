aed281358f9db97268d29fb3c3a753ecdcd76304
Godwin Boateng <2425403236@live.gctu.edu.gh>
2026-05-30 10:10:35 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,29 @@
+# File: initials.py
+# Description: Print out my initials in stylized large block letters.
+# Assignment Number: 1
+#
+# Name: Godwin Boateng Agyapong
+# STUDENT ID: 2425403236
+# Email: 2425403236@live.gctu.edu.gh
+# Grader: Augustus
+#
+# On my honor, Godwin Boateng Agyapong, this programming assignment is my own work
+# and I have not provided this code to any other student.
 
 def main():
-  pass #code goes here
-
-
+    print()
+    print("...GBA")
+    print()
+    print("...GGGGGGGGGGGG........BBBBBBBBBB.............AAAAAA........")
+    print("...GGGGGGGGGGGG........BBBBBBBBBBBB..........AAA..AAA.......")
+    print("...GGG.................BBB......BBB.........AAA....AAA......")
+    print("...GGG.................BBB......BBB........AAA......AAA.....")
+    print("...GGG.................BBBBBBBBBB..........AAAAAAAAAAAA.....")
+    print("...GGG..GGGGGGG........BBBBBBBBBB..........AAAAAAAAAAAA.....")
+    print("...GGG..GGGGGGG........BBB......BBB........AAA......AAA.....")
+    print("...GGG......GGG........BBB......BBB........AAA......AAA.....")
+    print("...GGGGGGGGGGGG...**...BBBBBBBBBBBB...**...AAA......AAA...**")
+    print("...GGGGGGGGGGGG...**...BBBBBBBBBB.....**...AAA......AAA...**")
+    print()
 
 main()
